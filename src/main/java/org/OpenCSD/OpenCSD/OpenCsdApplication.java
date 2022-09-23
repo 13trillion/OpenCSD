dfd4ddc1bc8d987db9ea4541ef65e58c9d0c4a41
Bilal Gutu <Bilalgutu13@gmail.com>
2022-09-22 23:25:07 -0400

    OpenCSD Baseskeleton

---
+++ src/main/java/org/OpenCSD/OpenCSD/OpenCsdApplication.java
@@ -0,0 +1,13 @@
+package org.OpenCSD.OpenCSD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenCsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenCsdApplication.class, args);
+	}
+
+}
